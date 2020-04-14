2dd8e86935e0c04b4ab14394f2db27e5244d831e
nikitha1311 <nikinikitha125@gmail.com>
2020-04-14 18:41:22 +0530

    initialcommit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+
+</head>
+<body>
+<div id="app">
+    @include('layouts.header')
+{{--    <main class="py-4 container" >--}}
+{{--        @yield('content')--}}
+{{--    </main>--}}
+
+@include('layouts.footer')
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+<html class="" lang="en">
+<head>
+
+</head>
+<body>
+<div class="home-banner">
+    <div class="container">
+        @include('layouts.header')
+        @include('layouts.footer')
+    </div>
+</div>
+</body>
+
