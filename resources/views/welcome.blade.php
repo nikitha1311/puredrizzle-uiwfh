6252ef92b26a9eb79fb85e1cc86aaa725afe18aa
nikitha1311 <nikinikitha125@gmail.com>
2020-04-15 12:37:01 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
  <!DOCTYPE html>
     <html class="" lang="en">
-    
+
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
@@ -8,7 +8,7 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick-theme.min.css" rel="stylesheet"/>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    
+
     <body>
         <div class="home-banner">
 
